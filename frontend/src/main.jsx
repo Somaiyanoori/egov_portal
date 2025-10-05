8f18998c346174fb4bf02da1b5524b2c76340e64
Somaiya Noori <somayenoori786@icloud.com>
2025-10-05 19:39:15 +0430

    bootstarp man.jxs

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,7 +8,7 @@ import { LanguageProvider } from "./context/LanguageContext.jsx";
 import { NotificationProvider } from "./context/NotificationContext.jsx";
 import App from "./App.jsx";
 import "./index.css";
-import "bootstrap/dist/css/bootstrap.rtl.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
